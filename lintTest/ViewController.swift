ba2520028ad6e7fe9c6bc2bb332b8c929f50d650
Hyunndy <yoosa3004@deali.net>
2022-09-02 18:46:34 +0900

    asdsadsd

--- lintTest/ViewController.swift
+++ lintTest/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var test:Any?
+    var st:Any?
     
     func abc(a:String, b:String) {
         print("dkdkdkdkdkdkdkdkdkasdjposadjopasdjopdsjop")
