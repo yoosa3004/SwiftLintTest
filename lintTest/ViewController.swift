9fda07915d9acdb1cfe8d86b44f300979344c68c
yoosa3004 <yoosa3004@deali.net>
2022-09-02 17:03:47 +0900

    Initial Commit

---
+++ lintTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lintTest
+//
+//  Created by hyunndy on 2022/09/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.아아아
+    }
+
+
+}
+
