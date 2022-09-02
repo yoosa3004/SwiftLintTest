44c70816d54ec49f5026f315d079d879b938a1c6
Hyunndy <yoosa3004@deali.net>
2022-09-02 17:36:06 +0900

    ㅁㄴㅇㄴㅁㅇ

--- lintTest/ViewController.swift
+++ lintTest/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     var test:Any?
     
     func abc(a:String, b:String) {
-        print("dkdkdkdkdkdkdkdkdk")
+        print("dkdkdkdkdkdkdkdkdk너엔머애ㅔㄴ머애ㅔㄴ머ㅐㅔ너ㅐㅔ어ㅐㅔ어ㅐㅔ")
     }
     
     override func viewDidLoad() {
