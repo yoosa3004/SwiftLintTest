c36e6aeb48120f8aaefc2e0680ce9e6d570264f6
Hyunndy <yoosa3004@deali.net>
2022-09-02 19:12:38 +0900

    ㅂㅉㅂㄹㅈㅂㄹㅂㄹ
    ㅂㅈ

---
+++ lintTest/ViewController2.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController2.swift
+//  lintTest
+//
+//  Created by hyunndy on 2022/09/02.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    let hi: String = "안녕하세요"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
